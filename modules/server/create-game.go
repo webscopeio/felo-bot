eac9660b1a5ae9fcb921361ad08655eb61abe073
samuelhulla <oficialny.hulla@gmail.com>
2025-01-10 11:06:18 +0100

    fix: nil pointer dereference

--- modules/server/create-game.go
+++ modules/server/create-game.go
@@ -15,7 +15,7 @@ func createGameHandler(ctx *gin.Context, slack *slack.Client, db *supabase.DB) {
 
 	resp, err := slack.CreateGame(triggerId, text)
 
-	fmt.Printf("CreateGame response: %v\nError: %v", resp, err.Error())
+	fmt.Printf("CreateGame response: %v", resp)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, Response{Status: "error", Ok: false, Message: "Error creating game: " + err.Error()})
 		return
